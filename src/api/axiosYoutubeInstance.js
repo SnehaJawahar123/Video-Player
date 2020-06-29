d2c1b75899bc03ccad2fa090b29ad022f635bd10
Jawahar <SNEHA.JAWAHAR@DISH.COM>
2020-06-29 13:21:03 +0530

    Revert "First Commit" This reverts commit 1b95dfbd7eea21fa0707fe75572c34a36abca0de.

--- src/api/axiosYoutubeInstance.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-// This is an axios instance we are creating
-
-const axiosYoutubeInstance = axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3'
-})
-
-export default axiosYoutubeInstance 
--- src/components/VideoList.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import VideoItem from './VideoItem'
-
-const videoList = ({videos, onVideoClicked})=>{
-    return(videos.map((video, id)=><VideoItem key={id} video={video} onVideoClicked={onVideoClicked}/>))
-}
-
-export default videoList 
